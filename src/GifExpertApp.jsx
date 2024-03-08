d14ab881b5c624694e9e0ceb9865c379e7e74dce
Ivan Pacheco <pachecomaldonadoivan@gmail.com>
2024-03-07 22:53:05 -0600

    method onAddCategory added

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ 'One Punch', 'Dragon ball']);
+
+    const onAddCategory = () => {
+        setCategories([...categories, 'Valorant']);
+    }
+
+    console.log(categories  )
+
     return (
         <>
+            {/* tittle */}
             <h1>GifExpertApp</h1>
+
+            {/* Input */}
+            {/* List of Gif */}
+            <button onClick={onAddCategory}>Agregar</button>
+            <ol>
+                { 
+                    categories.map( category => {
+                        return <li key= { category }>Hola</li>
+                    }) 
+                }
+            </ol>
+                {/* Gif item */}
         </>
     )
 }
