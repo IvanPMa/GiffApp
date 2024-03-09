e6847d7e8da378a3d55630956e7306bf6df64a2d
Ivan Pacheco <pachecomaldonadoivan@gmail.com>
2024-03-08 21:32:57 -0600

    Validation unique category name

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -7,6 +7,8 @@ export const GifExpertApp = () => {
 
     const onAddCategory = ( newCategory ) => {
         //console.log( newCategory );
+
+        if(categories.includes( newCategory )) return;
         setCategories([ newCategory,...categories ]);
     }
 
